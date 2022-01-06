27ff7c251d1bb705f4ebcff24fd67a86288bd52b
ZamIzzudin <azzamizzudinhasan@gmail.com>
2022-01-06 19:42:55 +0700

    optimize performance web

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 // FUNCTiON
 function getHTML(html) {
     return document.querySelector(html);
@@ -8,12 +7,12 @@ function getHTMLs(htmls) {
     return document.querySelectorAll(htmls);
 }
 
-function rendersX(e){
-    e.forEach((el,index)=>{
+function rendersX(e) {
+    e.forEach((el, index) => {
         setTimeout(() => {
             el.style.transform = `translateX(0)`;
-            el.style.opacity = 1;  
-        }, 300 * index);      
+            el.style.opacity = 1;
+        }, 300 * index);
     })
 }
 
@@ -27,41 +26,19 @@ function rendersY(e) {
     console.log("nyala")
 }
 
-function fadesX(e){
-    e.forEach((el, index) => {
-        setTimeout(() => {
-            el.style.transform = `translateX(50%)`;
-            el.style.opacity = 0;
-        }, 300 * index);
-    })
-}
-
-function fadesY(e) {
-    e.forEach((el, index) => {
-        setTimeout(() => {
-            el.style.transform = `translateY(30vh)`;
-            el.style.opacity = 0;
-        }, 300 * index);
-    })
-}
-
-function renderY(e,opacity){
-    e.style.transform =`translateY(0)`;
+function renderY(e, opacity) {
+    e.style.transform = `translateY(0)`;
     e.style.opacity = opacity;
 }
 
-function fadeY(e){
-    e.style.transform = `translateY(100%)`;
-}
-
-function getDataProject(url,success){
+function getDataProject(url, success) {
     let dataProject = new XMLHttpRequest();
 
     dataProject.onreadystatechange = () => {
-        if(dataProject.readyState === 4){
-            if(dataProject.status === 200){
+        if (dataProject.readyState === 4) {
+            if (dataProject.status === 200) {
                 success(dataProject.response);
-            }else if(dataProject.status === 404){
+            } else if (dataProject.status === 404) {
                 alert('data not found');
             }
         }
@@ -71,18 +48,18 @@ function getDataProject(url,success){
     dataProject.send();
 }
 
-function success(result){
+function success(result) {
     let data = JSON.parse(result);
 
     renderData = data.slice(0, 4);
     linkData = data[4];
-    
-    renderData.forEach((e,i)=>{
-        if(i % 2 === 0){
+
+    renderData.forEach((e, i) => {
+        if (i % 2 === 0) {
             render = renderCard(e, i, "");
             projectContainer.innerHTML += render;
-        }else{
-            render = renderCard(e, i, "right");  
+        } else {
+            render = renderCard(e, i, "right");
             projectContainer.innerHTML += render;
         }
     })
@@ -91,7 +68,7 @@ function success(result){
 
 }
 
-function renderCard(data,index,right){
+function renderCard(data, index, right) {
     return `<div class="project-card ${right}">
                 <div class="project-viewport">
                     <div class="project-desc">
@@ -100,7 +77,7 @@ function renderCard(data,index,right){
                     </div>
                     <img src="${data.thumbnail}" alt="Thumbnail picture of ${data.name}" >
                     <div class="project-info">
-                        <span class="project-num">0${index +1}.</span>
+                        <span class="project-num">0${index + 1}.</span>
                         <h1 class="project-name">${data.name}</h1>
                         <h2 class="project-tool">${data.tool}</h2>
                     </div>
@@ -108,7 +85,7 @@ function renderCard(data,index,right){
             </div>`
 }
 
-function renderLink(data){
+function renderLink(data) {
     return `<a class="project-btn" href="${data.link}">See My ${data.name} <i class="fas fa-long-arrow-alt-right arrow"></i></a>`
 }
 
@@ -129,60 +106,40 @@ const projectViewport = getHTML('.project-viewport');
 
 const projectDesc = getHTML('.project-desc');
 
-const preloader = getHTML('#preloader');    
+const preloader = getHTML('#preloader');
 
-getDataProject('./data.json', success); 
+getDataProject('./data.json', success);
 
 // EVENT LISTENER
-document.addEventListener("scroll",()=>{
+document.addEventListener("scroll", () => {
     let scrollY = window.pageYOffset;
 
     const projectTitle = getHTMLs('.project-title .section-title');
     const projectCard = getHTMLs('.project-viewport');
 
-    if(scrollY < 500){
-        titleFP[0].style.transform = `translateX(${-80 - (scrollY/10)}%)`;
+    if (scrollY < 500) {
+        titleFP[0].style.transform = `translateX(${-80 - (scrollY / 10)}%)`;
         titleOutFP[0].style.transform = `translateX(${-80 - (scrollY / 10)}%)`;
         titleFP[1].style.transform = `translateX(${-160 + (scrollY / 10)}%)`;
         titleOutFP[1].style.transform = `translateX(${-160 + (scrollY / 10)}%)`;
     }
 
-    if(scrollY > 500){
-        rendersX(projectTitle);
-        aboutBtn.style.opacity = 0;
-    }else{
-        fadesX(projectTitle);
-        aboutBtn.style.opacity = 1;
-    }
-
-    if(window.screen.width > 1023){
+    if (window.screen.width > 1023) {
         if (scrollY > 500) {
             rendersX(projectTitle);
-            aboutBtn.style.opacity = 0;
-        } else {
-            fadesX(projectTitle);
-            aboutBtn.style.opacity = 1;
         }
 
         if (scrollY > 900) {
             renderY(projectCard[0], 1);
             if (scrollY > 1400) {
                 renderY(projectCard[1], 1);
-                if (scrollY > 1800) {
+                if (scrollY > 1900) {
                     renderY(projectCard[2], 1);
-                    if (scrollY > 2300) {
+                    if (scrollY > 2400) {
                         renderY(projectCard[3], 1);
-                    } else {
-                        fadeY(projectCard[3]);
                     }
-                } else {
-                    fadeY(projectCard[2]);
                 }
-            } else {
-                fadeY(projectCard[1]);
             }
-        } else {
-            fadeY(projectCard[0]);
         }
 
         if (scrollY > 2900) {
@@ -190,18 +147,11 @@ document.addEventListener("scroll",()=>{
             setTimeout(() => {
                 renderY(contactImg, .7);
             }, 300);
-        } else {
-            fadesY(contactTitle);
-            fadeY(contactImg);
         }
 
-    }else{
+    } else {
         if (scrollY > 300) {
             rendersX(projectTitle);
-            aboutBtn.style.opacity = 0;
-        } else {
-            fadesX(projectTitle);
-            aboutBtn.style.opacity = 1;
         }
 
         if (scrollY > 400) {
@@ -212,17 +162,9 @@ document.addEventListener("scroll",()=>{
                     renderY(projectCard[2], 1);
                     if (scrollY > 1800) {
                         renderY(projectCard[3], 1);
-                    } else {
-                        fadeY(projectCard[3]);
                     }
-                } else {
-                    fadeY(projectCard[2]);
                 }
-            } else {
-                fadeY(projectCard[1]);
             }
-        } else {
-            fadeY(projectCard[0]);
         }
 
         if (scrollY > 2400) {
@@ -230,24 +172,21 @@ document.addEventListener("scroll",()=>{
             setTimeout(() => {
                 renderY(contactImg, .7);
             }, 300);
-        } else {
-            fadesY(contactTitle);
-            fadeY(contactImg);
         }
     }
 })
 
-window.addEventListener("load", ()=>{
+window.addEventListener("load", () => {
     setTimeout(() => {
         preloader.style.opacity = 0;
         setTimeout(() => {
-            preloader.style.display = "none";        
-        }, 700);  
+            preloader.style.display = "none";
+        }, 700);
     }, 2000);
 })
 
-if (window.screen.width > 1023){
-    projectViewport.addEventListener("click",()=>{
+if (window.screen.width > 1023) {
+    projectViewport.addEventListener("click", () => {
         projectDesc.style.opacity = 1;
     })
 }
